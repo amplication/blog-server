43acf67fa0fb0683ec8a2d051fec2eb52dd479c7
Shahar <shaharb@sela.co.il>
2022-09-13 14:44:06 +0300

    delete client mapping

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,18 +149,6 @@ resource "google_cloud_run_service_iam_member" "run_all_client_users" {
   member   = "allUsers"
 }
 
-resource "google_cloud_run_domain_mapping" "client_mapping" {
-  location = var.region
-  name     = var.blog_server_client_domain
-
-  metadata {
-    namespace = var.project_id
-  }
-
-  spec {
-    route_name = google_cloud_run_service.client-service.name
-  }
-}
 resource "google_cloud_run_domain_mapping" "server_mapping" {
   location = var.region
   name     = var.blog_server_domain
