ecafb11e72d1302a1222a9adc85dcca467d8491c
Michael Solati <mkslt04@gmail.com>
2022-09-19 10:01:42 -0700

    feat(slugify): add constant options for slugify

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,2 +1,7 @@
 export const JWT_SECRET_KEY = "JWT_SECRET_KEY";
 export const JWT_EXPIRATION = "JWT_EXPIRATION";
+export const SLUGGIFY_OPTIONS = {
+  lower: true,
+  strict: true,
+  locale: "en",
+};
--- server/src/tag/tag.service.ts
+++ server/src/tag/tag.service.ts
@@ -3,6 +3,7 @@ import { Prisma, Tag } from "@prisma/client";
 import { PrismaService } from "nestjs-prisma";
 import slugify from "slugify";
 import { TagServiceBase } from "./base/tag.service.base";
+import { SLUGGIFY_OPTIONS } from "../constants";
 
 @Injectable()
 export class TagService extends TagServiceBase {
@@ -13,17 +14,17 @@ export class TagService extends TagServiceBase {
   async create<T extends Prisma.TagCreateArgs>(
     args: Prisma.SelectSubset<T, Prisma.TagCreateArgs>
   ): Promise<Tag> {
-    args.data.slug = slugify(args.data.name ?? '');
+    args.data.slug = slugify(args.data.name ?? '', SLUGGIFY_OPTIONS);
     return super.create(args);
   }
 
   async update<T extends Prisma.TagUpdateArgs>(
     args: Prisma.SelectSubset<T, Prisma.TagUpdateArgs>
   ): Promise<Tag> {
-    if (!args.data.slug) {
+    if (args.data.slug && typeof args.data.slug !== 'string') {
       delete args.data.slug;
     } else if (args.data.slug) {
-      args.data.slug = slugify(args.data.slug);
+      args.data.slug = slugify(args.data.slug, SLUGGIFY_OPTIONS);
     }
     return super.update(args);
   }
